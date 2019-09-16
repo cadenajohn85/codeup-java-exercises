51a5a49fa43e837f46555bafe17061e42fc067a3
John Cadena <cadena.john.85@gmail.com>
2019-09-16 16:21:53 -0500

    refactor HelloWorld.java for Syntax, Types, and Variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,129 @@
 public class HelloWorld {
     public static void main(String[] args) {
+
+        // Introdution to Java
+
 //        System.out.println("Hello, World!");
-        System.out.print("Hello, ");
-        System.out.print("World!");
+//        System.out.print("Hello, ");
+//        System.out.print("World!");
+
+
+        // Syntax, Types, and Variables
+
+        // Problem 1
+//        int myFavoriteNumber = 14;
+//        System.out.println(myFavoriteNumber);
+
+        // Problem 2
+//        String myString = "This is my first Java String.";
+//        System.out.println(myString);
+
+        // Problem 3
+//        myString = "a";
+//        System.out.println(myString);
+//        myString = 'a';
+
+        // Problem 4
+//        myString = 3.14159;
+
+        // Problem 5
+//        long myNumber;
+//        System.out.println(myNumber);
+
+        // Problem 6
+//        myNumber = 3.14;
+
+        // Problem 7
+//        myNumber = 123L;
+//        System.out.println(myNumber);
+
+        // Problem 8
+
+//        myNumber = 123;
+//        System.out.println(myNumber);
+
+        // Problem 9
+
+//        float myNewNumber = 3.14;
+//        float myNewNumber = 3.14F;
+//        System.out.println(myNewNumber);
+
+//        double myNewerNumber = 3.14;
+//        System.out.println(myNewerNumber);
+
+        // Problem 10
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        // Problem 11
+
+//        boolean class = true;
+
+        // Problem 12
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+        // Problem 13
+
+//        int x = 4;
+//        x = x + 5;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+////        y = y * x;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+        // Problem #14
+
+        int tooBig = Integer.MAX_VALUE + 1;
+        System.out.println(tooBig);
+
+        byte almostTooBig = 126;
+        System.out.println(almostTooBig);
+        almostTooBig++;
+        System.out.println(almostTooBig);
+        almostTooBig++;
+        System.out.println(almostTooBig);
+
+        tooBig += 2147483647;
+        System.out.println(tooBig);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
