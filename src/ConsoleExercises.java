35c62d2fa88df13bb6ff8983b1e86401cc3c184e
John Cadena <cadena.john.85@gmail.com>
2019-09-17 10:57:17 -0500

    complete bonuses for HelloWorld.java

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -8,6 +8,7 @@ public class ConsoleExercises {
 //        System.out.format("The value of pi is approximately %.2f%n", pi);
 
         Scanner input = new Scanner(System.in);
+        input.useDelimiter("\n");
 
 //        System.out.println("Please enter an integer:");
 //        int userInt = input.nextInt();
@@ -25,14 +26,39 @@ public class ConsoleExercises {
 //        String userSentence = input.nextLine();
 //        System.out.println(userSentence);
 
-        System.out.println("Please enter the length and width of your classroom:");
-        String length = input.next();
-        String width = input.next();
-        int numLength = Integer.parseInt(length);
-        int numWidth = Integer.parseInt(width);
-        int perimeter = 2 * numLength + 2 * numWidth;
-        int area = numLength * numWidth;
-        System.out.format("The perimeter of the room is %s units, and the area of the room is %s units.%n", perimeter, area);
+//        System.out.println("Please enter the length and width of your classroom:");
+//        String length = input.next();
+//        String width = input.next();
+//        int numLength = Integer.parseInt(length);
+//        int numWidth = Integer.parseInt(width);
+//        int perimeter = 2 * numLength + 2 * numWidth;
+//        int area = numLength * numWidth;
+//        System.out.format("The perimeter of the room is %s units, and the area of the room is %s units.%n", perimeter, area);
+
+//        System.out.println("Please enter the length, width, and height of your classroom:");
+//        String length = input.next();
+//        String width = input.next();
+//        String height = input.next();
+//        double numLength = Double.parseDouble(length);
+//        double numWidth = Double.parseDouble(width);
+//        double numHeight = Double.parseDouble((height));
+//        double perimeter = 2 * numLength + 2 * numWidth;
+//        double area = numLength * numWidth;
+//        double volume = numLength * numWidth * numHeight;
+//        System.out.format("The perimeter of the room is %.2f units.%n" +
+//                "The area of the room is %.2f units.%n" +
+//                "The volume of the room is %.2f units.%n", perimeter, area, volume);
+
+        System.out.println("Please enter the length, width, and height of your classroom:");
+        int length = input.nextInt();
+        int width = input.nextInt();
+        int height = input.nextInt();
+        int perimeter = 2 * length + 2 * width;
+        int area = length * width;
+        int volume = length * width * height;
+        System.out.format("The perimeter of the room is %s units.%n" +
+                "The area of the room is %s units.%n" +
+                "The volume of the room is %s units.%n", perimeter, area, volume);
 
 
 
