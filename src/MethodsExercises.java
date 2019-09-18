cd91bebfbefbbcfb1e9fb0568cf0abb03f1f4b2d
John Cadena <cadena.john.85@gmail.com>
2019-09-18 15:54:21 -0500

    complete MethodsExercises.java Problem #1 Bonuses

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -12,11 +12,11 @@ public class MethodsExercises {
         return num1 * num2;
     }
 
-//    public static double divide(double num1, double num2) {
-//        return num1 / num2;
-//    }
+    public static double divideDoubles(double num1, double num2) {
+        return num1 / num2;
+    }
 
-    public static int divide(int num1, int num2) {
+    public static int divideInts(int num1, int num2) {
         if (num2 == 0) {
             // Can't return an error message;
             return 55555;
@@ -29,18 +29,56 @@ public class MethodsExercises {
         return num1 % num2;
     }
 
+    public static int bonusMult(int num1, int num2) {
+        int total = 0;
+        for (int i = 0; i < java.lang.Math.abs(num1); i++) {
+            total += num2;
+        }
+        if (num1 != java.lang.Math.abs(num1)) {
+            total = 0 - total;
+        }
+        return total;
+    }
+
+    public static int recursiveMult(int num1, int num2) {
+        // If both numbers are negative, turn them positive
+        if (Integer.signum(num1) == -1 && Integer.signum(num2) == -1) {
+            num1 = java.lang.Math.abs(num1);
+            num2 = java.lang.Math.abs(num2);
+        }
+        // If number2 is negative, avoid stack overflow by swapping values of num1 and num2
+        if (Integer.signum(num2) == -1) {
+            int placeholder = num1;
+            num1 = num2;
+            num2 = placeholder;
+        }
+        // Normal recursive conditions
+        if (num2 == 0) {
+            return 0;
+        }
+        return num1 + recursiveMult(num1, num2 - 1);
+    }
+
     public static void main(String[] Args) {
 //        System.out.println(add(3, 7));
 //        System.out.println(subtract(3, 7));
 //        System.out.println(multiply(3, 7));
-        // Divide only returns the quotient if the parameters are defined as int, even if the method returns a double.
-//        System.out.println(divide(3, 7));
-//        System.out.println(divide(8, 2));
-//        System.out.println(divide(8, 3));
-//        System.out.println(modulo(3, 7));
-//        System.out.println(modulo(8, 2));
+    // Divide only returns the quotient if the parameters are defined as int, even if the method returns a double.
+//        System.out.println(divideDoubles(3, 7));
+//        System.out.println(divideInts(3, 7));
 //        System.out.println(modulo(8, 3));
-        System.out.println(divide(8, 0));
+
+        System.out.println(divideDoubles(8, 0));
+        System.out.println(divideInts(8, 0));
+
+//        System.out.println(bonusMult(4, 7));
+//        System.out.println(recursiveMult(9, -5));
+
+
+
+
+
+
 
 
 
