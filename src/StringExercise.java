bacd7786fede3892220131bcc11e7031b0ad4110
John Cadena <cadena.john.85@gmail.com>
2019-09-18 11:49:11 -0500

    create initial StringExercise.java class

---
+++ src/StringExercise.java
@@ -0,0 +1,19 @@
+public class StringExercise {
+
+    public static void main(String[] args) {
+
+        // Pig Latin pre-exercise challenge
+
+//        String cat = "cat";
+//        String firstLetter = String.valueOf(cat.charAt(0));
+//        String firstLetterGone = cat.substring(1);
+//        System.out.println(firstLetterGone + firstLetter + "ay");
+
+
+
+
+
+
+    }
+
+}
