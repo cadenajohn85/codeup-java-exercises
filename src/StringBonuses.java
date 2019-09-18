82a0fa905765cd3875882a09342a6fd70bfd56da
John Cadena <cadena.john.85@gmail.com>
2019-09-18 14:17:31 -0500

    Complete StringBonuses.java Problem #2

--- src/StringBonuses.java
+++ src/StringBonuses.java
@@ -22,57 +22,57 @@ public class StringBonuses {
 //        input - 12/01/1999
 //        output - December 1, 1999
 
-        System.out.println("Date Converter - Please enter a date in MM/DD/YYYY format:");
-        String userDate = input.nextLine();
-
-        int userMonth = Integer.parseInt(userDate.substring(0, userDate.indexOf("/")));
-        int userDayOfMonth = Integer.parseInt(userDate.substring(userDate.indexOf("/") + 1, userDate.lastIndexOf("/")));
-        int userYear = Integer.parseInt(userDate.substring(userDate.lastIndexOf("/") + 1));
-
-        String formattedMonth;
-
-        switch (userMonth) {
-            case 1:
-                formattedMonth = "January";
-                break;
-            case 2:
-                formattedMonth = "February";
-                break;
-            case 3:
-                formattedMonth = "March";
-                break;
-            case 4:
-                formattedMonth = "April";
-                break;
-            case 5:
-                formattedMonth = "May";
-                break;
-            case 6:
-                formattedMonth = "June";
-                break;
-            case 7:
-                formattedMonth = "July";
-                break;
-            case 8:
-                formattedMonth = "August";
-                break;
-            case 9:
-                formattedMonth = "September";
-                break;
-            case 10:
-                formattedMonth = "October";
-                break;
-            case 11:
-                formattedMonth = "November";
-                break;
-            case 12:
-                formattedMonth = "December";
-                break;
-            default:
-                formattedMonth = "(Invalid month entry)";
-        }
-
-        System.out.format("%s %d, %d", formattedMonth, userDayOfMonth, userYear);
+//        System.out.println("Date Converter - Please enter a date in MM/DD/YYYY format:");
+//        String userDate = input.nextLine();
+//
+//        int userMonth = Integer.parseInt(userDate.substring(0, userDate.indexOf("/")));
+//        int userDayOfMonth = Integer.parseInt(userDate.substring(userDate.indexOf("/") + 1, userDate.lastIndexOf("/")));
+//        int userYear = Integer.parseInt(userDate.substring(userDate.lastIndexOf("/") + 1));
+//
+//        String formattedMonth;
+//
+//        switch (userMonth) {
+//            case 1:
+//                formattedMonth = "January";
+//                break;
+//            case 2:
+//                formattedMonth = "February";
+//                break;
+//            case 3:
+//                formattedMonth = "March";
+//                break;
+//            case 4:
+//                formattedMonth = "April";
+//                break;
+//            case 5:
+//                formattedMonth = "May";
+//                break;
+//            case 6:
+//                formattedMonth = "June";
+//                break;
+//            case 7:
+//                formattedMonth = "July";
+//                break;
+//            case 8:
+//                formattedMonth = "August";
+//                break;
+//            case 9:
+//                formattedMonth = "September";
+//                break;
+//            case 10:
+//                formattedMonth = "October";
+//                break;
+//            case 11:
+//                formattedMonth = "November";
+//                break;
+//            case 12:
+//                formattedMonth = "December";
+//                break;
+//            default:
+//                formattedMonth = "(Invalid month entry)";
+//        }
+//
+//        System.out.format("%s %d, %d", formattedMonth, userDayOfMonth, userYear);
 
 //
 //
@@ -85,6 +85,47 @@ public class StringBonuses {
 //
 //        input - "The hill are alive."
 //        output - 5 vowels and 10 consonants
+
+        System.out.println("Please enter a sentence:");
+        String userSentence = input.nextLine();
+
+        int vowelCounter = 0;
+        int consonantCounter = 0;
+
+        for (int i = 0; i < userSentence.length(); ++i) {
+            if (userSentence.toLowerCase().charAt(i) == 'a' ||
+                    userSentence.toLowerCase().charAt(i) == 'e' ||
+                    userSentence.toLowerCase().charAt(i) == 'i' ||
+                    userSentence.toLowerCase().charAt(i) == 'o' ||
+                    userSentence.toLowerCase().charAt(i) == 'u') {
+                vowelCounter++;
+            } else if (userSentence.toLowerCase().charAt(i) == 'b' ||
+                    userSentence.toLowerCase().charAt(i) == 'c' ||
+                    userSentence.toLowerCase().charAt(i) == 'd' ||
+                    userSentence.toLowerCase().charAt(i) == 'f' ||
+                    userSentence.toLowerCase().charAt(i) == 'g' ||
+                    userSentence.toLowerCase().charAt(i) == 'h' ||
+                    userSentence.toLowerCase().charAt(i) == 'j' ||
+                    userSentence.toLowerCase().charAt(i) == 'k' ||
+                    userSentence.toLowerCase().charAt(i) == 'l' ||
+                    userSentence.toLowerCase().charAt(i) == 'm' ||
+                    userSentence.toLowerCase().charAt(i) == 'n' ||
+                    userSentence.toLowerCase().charAt(i) == 'p' ||
+                    userSentence.toLowerCase().charAt(i) == 'q' ||
+                    userSentence.toLowerCase().charAt(i) == 'r' ||
+                    userSentence.toLowerCase().charAt(i) == 's' ||
+                    userSentence.toLowerCase().charAt(i) == 't' ||
+                    userSentence.toLowerCase().charAt(i) == 'v' ||
+                    userSentence.toLowerCase().charAt(i) == 'w' ||
+                    userSentence.toLowerCase().charAt(i) == 'x' ||
+                    userSentence.toLowerCase().charAt(i) == 'y' ||
+                    userSentence.toLowerCase().charAt(i) == 'z') {
+                consonantCounter++;
+            }
+        }
+        System.out.format("%d vowels and %d consonants", vowelCounter, consonantCounter);
+
+
 //
 //
 //                ----BONUS 3
