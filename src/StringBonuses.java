a8868d0b0194d4e8ddd1b805594bee648e8a3125
John Cadena <cadena.john.85@gmail.com>
2019-09-18 14:04:28 -0500

    complete StringBonuses.java Problem #1

--- src/StringBonuses.java
+++ src/StringBonuses.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class StringBonuses {
 
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
 
 //                ----BONUS 1
 //
@@ -16,7 +20,60 @@ public class StringBonuses {
 //        Example:
 //
 //        input - 12/01/1999
-//        output - December 12, 1999
+//        output - December 1, 1999
+
+        System.out.println("Date Converter - Please enter a date in MM/DD/YYYY format:");
+        String userDate = input.nextLine();
+
+        int userMonth = Integer.parseInt(userDate.substring(0, userDate.indexOf("/")));
+        int userDayOfMonth = Integer.parseInt(userDate.substring(userDate.indexOf("/") + 1, userDate.lastIndexOf("/")));
+        int userYear = Integer.parseInt(userDate.substring(userDate.lastIndexOf("/") + 1));
+
+        String formattedMonth;
+
+        switch (userMonth) {
+            case 1:
+                formattedMonth = "January";
+                break;
+            case 2:
+                formattedMonth = "February";
+                break;
+            case 3:
+                formattedMonth = "March";
+                break;
+            case 4:
+                formattedMonth = "April";
+                break;
+            case 5:
+                formattedMonth = "May";
+                break;
+            case 6:
+                formattedMonth = "June";
+                break;
+            case 7:
+                formattedMonth = "July";
+                break;
+            case 8:
+                formattedMonth = "August";
+                break;
+            case 9:
+                formattedMonth = "September";
+                break;
+            case 10:
+                formattedMonth = "October";
+                break;
+            case 11:
+                formattedMonth = "November";
+                break;
+            case 12:
+                formattedMonth = "December";
+                break;
+            default:
+                formattedMonth = "(Invalid month entry)";
+        }
+
+        System.out.format("%s %d, %d", formattedMonth, userDayOfMonth, userYear);
+
 //
 //
 //                ----BONUS 2
