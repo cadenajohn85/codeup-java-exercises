37a142648b00a226ae1c5dfc1ee6e2af2f06002c
John Cadena <cadena.john.85@gmail.com>
2019-09-23 09:34:58 -0500

    debug HighLow.java whitespace issue by changing .NextLine to .Next

--- src/BonusSandbox.java
+++ src/BonusSandbox.java
@@ -4,34 +4,10 @@ import java.util.Scanner;
 
 public class BonusSandbox {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter a number between 1 and 10");
-        int userNum = getInt(1, 10);
-        System.out.println("Will we ever get to this text?");
-        System.out.println("how about this text?");
-        String secondUser = sc.nextLine();
-        System.out.println(secondUser);
-
-    }
-
-    // with recursion
-
-    public static int getInt(int min, int max) {
-        Scanner sc = new Scanner(System.in);
-        if (sc.hasNextInt()) {
-            int userNum = sc.nextInt();
-            if (userNum >= min && userNum <= max) {
-                return userNum;
-            } else {
-                System.out.println("Invalid range");
-                return getInt(min, max);
-            }
-        } else {
-            System.out.println("Invalid Input!");
-            return getInt(min, max);
-        }
-
-    }
-
+     public static void main(String[] args) {
+                     int x = 5;
+                     System.out.println(x > 4 ? x < 2 ? 10 : 8 : 7);
+                     //             Both are true : only first is true : first is false
+                 }
 }
+
--- src/HighLow.java
+++ src/HighLow.java
@@ -2,27 +2,6 @@ import java.util.Scanner;
 
 public class HighLow {
 
-    // Problem 5: Game Development 101
-
-//    Welcome to the world of game development!
-//    You are going to build a high-low guessing game. Create a class named HighLow inside of src.
-//    The specs for the game are:
-//
-//    Game picks a random number between 1 and 100.
-//    Prompts user to guess the number.
-//    All user inputs are validated.
-//    If user's guess is less than the number, it outputs "HIGHER".
-//    If user's guess is more than the number, it outputs "LOWER".
-//    If a user guesses the number, the game should declare "GOOD GUESS!"
-//    Hints
-//
-//    Use the random method of the java.lang.Math class to generate a random number.
-//    Bonus
-//
-//    Keep track of how many guesses a user makes.
-//    Set an upper limit on the number of guesses.
-
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to HighLow! I'm thinking of a number between 1 and 100. Guess my number:");
@@ -34,7 +13,7 @@ public class HighLow {
         int randomNumber = (int) (Math.random() * 100) + 1;
         validateUserGuess(randomNumber, input, guessCounter);
         System.out.println("Would you like to play again? [ y | n ]");
-        if (input.nextLine().toLowerCase().equals("y")) {
+        if (input.next().toLowerCase().equals("y")) {
             System.out.println("I'm thinking of a number between 1 and 100. Guess my number:");
             playGame(input);
         } else {
@@ -43,24 +22,15 @@ public class HighLow {
     }
 
     public static void validateUserGuess(int randomNumber, Scanner input, int guessCounter) {
-        // Bugs:
-        // * Fix empty strings
-        // * Fix extra whitespace
-//        if (input.nextLine().isEmpty()) {
-//            input.nextLine(); // This clear the scanner of invalid input.
-//            System.out.println("Invalid input: You may not enter an empty return.");
-//            validateUserGuess(randomNumber, input);
-//        }
-
         ++guessCounter;
         // Confirm user guess is integer
         if (!input.hasNextInt()) {
-            input.nextLine(); // This clear the scanner of invalid input.
+            input.next(); // This clear the scanner of invalid input.
             System.out.println("Invalid input: Your guess must be an integer between 1 and 100.");
             validateUserGuess(randomNumber, input, guessCounter);
         } else {
             // Confirm user guess is in range
-            int userInput = Integer.parseInt(input.nextLine());
+            int userInput = Integer.parseInt(input.next());
             if (userInput < 1 || userInput > 100) {
                 System.out.println("Invalid input: Your guess must be between 1 and 100.");
                 validateUserGuess(randomNumber, input, guessCounter);
