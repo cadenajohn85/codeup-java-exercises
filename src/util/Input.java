461ec4decb4742fd8a076302dae9804e670691c6
John Cadena <cadena.john.85@gmail.com>
2019-09-24 14:41:03 -0500

    save progress on Input.java Problem #3

--- src/util/Input.java
+++ src/util/Input.java
@@ -15,39 +15,34 @@ public class Input {
     }
 
     public boolean yesNo() {
-        return (scanner.nextLine().equalsIgnoreCase("yes") ||
-                scanner.nextLine().equalsIgnoreCase("y"));
+        String userString = scanner.nextLine();
+        return (userString.equalsIgnoreCase("yes") ||
+                userString.equalsIgnoreCase("y"));
     }
 
     public int getInt() {
-        if (scanner.hasNextInt()) {
-            return scanner.nextInt();
-        } else {
-            return getInt();
-        }
+        return scanner.nextInt();
     }
 
     public int getInt(int min, int max) {
-        if (scanner.hasNextInt() && scanner.nextInt() >= min && scanner.nextInt() <= max) {
-            return scanner.nextInt();
-        } else {
+        int userInt = Integer.parseInt(scanner.nextLine());
+        if (userInt < min || userInt > max) {
             return getInt(min, max);
+            } else {
+                return userInt;
+            }
         }
-    }
 
     public double getDouble() {
-        if (scanner.hasNextDouble()) {
-            return scanner.nextDouble();
-        } else {
-            return getDouble();
-        }
+        return scanner.nextDouble();
     }
 
     public double getDouble(double min, double max) {
-        if (scanner.hasNextDouble() && scanner.nextDouble() >= min && scanner.nextDouble() <= max) {
-            return scanner.nextDouble();
-        } else {
+        double userDouble = Double.parseDouble(scanner.nextLine());
+        if (userDouble < min || userDouble > max) {
             return getDouble(min, max);
+        } else {
+            return userDouble;
         }
     }
 
